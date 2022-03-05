081b2c46c8896a291973016abace97cd8bf4318f
Md. Rabiul Islam <rabiul15@cse.pstu.ac.bd>
2022-03-05 10:59:36 +0600

    57. print random value from associative array

--- php056.php
+++ php056.php
@@ -1,1 +1,10 @@
 <?php
+
+$fruits = array('a' => 'apple', 'b' => 'banana', 'o' => 'orange', 'p' => 'plum', 'd' => 'dates', 'm' => 'mango');
+// shuffle($fruits);
+// print_r($fruits); // associative array : shuffled but key is not preserved!!
+
+$key = array_rand($fruits);
+// echo $key; // returns a random key without modifying the main array
+// echo PHP_EOL;
+echo $fruits[$key];
